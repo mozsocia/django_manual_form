0c2ac8e5e9b8d3783ccb8b9dc5fd65f88c952298
Mozdalif sikder <mozsocia@gmail.com>
2024-02-17 19:34:15 +0600

    edit page

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,6 +1,6 @@
 # views.py
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import *
 from .forms import *  # You'll need to create a form for creating blogs
@@ -21,3 +21,20 @@ def create(request):
         form = BlogForm()
 
     return render(request, 'blog/create.html', {'form': form})
+
+
+
+def edit(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+
+    if request.method == 'POST':
+        form = BlogForm(request.POST, instance=blog)
+        if form.is_valid():
+            blog = form.save(commit=False)
+            blog.pub_date = timezone.now()
+            blog.save()
+            return redirect('your_app_name:index')
+    else:
+        form = BlogForm(instance=blog)
+
+    return render(request, 'your_app_name/edit.html', {'form': form, 'blog': blog})
